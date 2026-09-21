9cf7592474733898ed50ebec16bd9d02890e922b
DavidPilahito7 <57416155+pilahito@users.noreply.github.com>
2026-09-21 20:37:48 +0200

    UI y permisos para actualizar desde CloudTerm.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pilahito.cloudterm.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.3.3"
+        versionCode = 10
+        versionName = "1.3.4"
     }
 
     signingConfigs {
