ceb996de0b312773fab0a43725f56a9a7e7011f8
DavidPilahito7 <57416155+pilahito@users.noreply.github.com>
2026-09-21 21:40:42 +0200

    version 1.3.6 UI video

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pilahito.cloudterm.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.3.5"
+        versionCode = 12
+        versionName = "1.3.6"
     }
 
     signingConfigs {
